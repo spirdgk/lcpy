fb72edf37053c99a710c75373f548b4ccf5ef77d
spiros gkousis <spirosg20@hotmail.gr>
2025-10-22 10:32:23 +0100

    extras_require in setup.py

--- setup.py
+++ setup.py
@@ -39,10 +39,10 @@ setup(
         "xlsxwriter"
     ],
     extras_require = {
-        "bw" = [
+        "bw": [
             "premise[bw2]",
-            "brightway2==2.4.7"
-        ]
+            "brightway2==2.4.7",
+        ],
     },
     python_requires=">=3.10", # python requirement
     license="BSD 3-Clause",
